c53664a3c22c54ad72283ffe36463ed291c3a233
sakimuzzaman <sakimuzzaman104@gmail.com>
2026-07-13 01:03:14 +0600

    Configuring the Environment variable with dotenv and adding express middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app";
+import config from "./config";
 import { prisma } from "./lib/prisma";
 
-const PORT = process.env.PORT || 5000; 
+const PORT = config.port;
 
 async function main() {
     try {
